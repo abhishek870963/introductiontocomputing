f8eca0764d337fd7f99111f77f839ed1fd1ff1ca
abhishek870963 <104257785+abhishek870963@users.noreply.github.com>
2022-04-23 23:36:48 +0530

    Add files via upload

---
+++ Assignment 01.(Output)py.py
@@ -0,0 +1,37 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.1).py
+ Please Enter the First Number: 10
+ Please Enter the second number: 12
+ Please Enter the third number: 14
+The average of three numbers is  12.0
+
+======================================== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.2).py =======================================
+Enter your gross income200000
+Enter number of dependents5
+Tax: 35000.0
+
+======================================== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.3).py =======================================
+Enter your SID: 21102127
+Enter your Name: Abhishek Narine
+Enter your Gender: M
+Enter your Course Name: Introduction to computing
+Enter your CGPA: 7.5
+Student: ['21102127', 'Abhishek Narine', 'M', 'Introduction to computing', '7.5']
+
+======================================== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.4).py =======================================
+Marks of 1st student: 45
+Marks of 2nd student: 34
+Marks of 3rd student: 67
+Marks of 4th student: 78
+Marks of 5th student: 56
+[45, 34, 67, 78, 56]
+
+======================================= RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.5a).py =======================================
+List of colors before removing:  ['Red', 'Green', 'White', 'Black', 'Pink', 'yellow']
+your list after removing 4th element:  ['Red', 'Green', 'White', 'Pink', 'yellow']
+
+======================================= RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/Assignment 01(Q.5b).py =======================================
+List of colors before replacing: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+your list after replacing black and pink with purple: ['Red', 'Green', 'White', 'Purple', 'Yellow']
