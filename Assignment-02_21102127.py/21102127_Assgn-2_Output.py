8eb34cd77f29f713b93d4cebe6289c9f8ec107b5
Abhishek Narine <104257785+abhishek870963@users.noreply.github.com>
2022-05-14 18:50:57 +0530

    Add files via upload

---
+++ Assignment-02_21102127.py/21102127_Assgn-2_Output.py
@@ -0,0 +1,42 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/21102127_Assgn-02.py
+Enter the string : Python is a case sensitive language
+Length of the given string:  35
+Reverse string:  egaugnal evitisnes esac a si nohtyP
+New string: a case sensitive
+New replaced string : Python is object oriented language
+Index of 'a': 10
+String without white spaces : Pythonisacasesensitivelanguage
+
+====== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\q.2.py =====
+Enter your name :Abhishek Narine
+Enter your sid :21102127
+Enter your department :CIVIL
+Enter your cgpa :8.0
+Hey, Abhishek Narine here!
+My SID is 21102127
+I am from CIVIL department and my CGPA is 8.000000
+
+====== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/q.3.py =====
+a&b : 8
+a|b : 58
+a^b : 50
+Left shift both a and b with 2 bits : a = %d, b = %d (224, 40)
+Right shift a with 2 bits and b with 4 bits : a = %d, b = %d (14, 0)
+
+====== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/q.4.py =====
+Enter a string My name is Abhishek Narine
+Yes
+
+===== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/q.5n.py =====
+10
+8
+9
+Yes
+
+====== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python310/q.6.py =====
+Enter number a:10
+Enter number b:20
+4
